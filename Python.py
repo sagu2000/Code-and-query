50e44776d028038feb830b091328f90ae1ff9057
sagu2000 <68247677+sagu2000@users.noreply.github.com>
2024-03-15 11:03:29 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,24 @@
+class Coins: 
+  def get change(self,x,y):
+    change round((x-y)*100)
+    coins [100,50,25,10,5,1] #in paise #coins 11.0.50.0.25.0.10.0.05.0.011
+
+    change coins [0]*len(coins)
+
+    for i in range(len(coins)): 
+       while change>= coins[i]:
+
+         change-=coins [i]
+
+         change_coins[i]+1
+
+    return change_coins[::-1]
+
+obj = Coins()
+
+#TastCases
+
+print(obj.get_change(5,0.99))
+print(obj.get_change(3.14,1.99))
+print(obj.get_change(4.3.14))
+print(obj.get_change(0.45,0.34))
